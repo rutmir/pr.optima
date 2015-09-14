731e4f9bb2a53fc151fc1f6a41d7e676a8849212
Roman Pankrushkin <roman.pankrushkin@datanaut.com>
2015-09-14 18:05:17 +0300

    cources enitity updated

--- src/server/rest.go/entities/Cources.go
+++ src/server/rest.go/entities/Cources.go
@@ -1,14 +1,23 @@
 package entities
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Cources struct {
-	Id         int        `json:"id"`
-	Base       string        `json:"base"`
-	Timestamp  uint64        `json:"timestamp"`
-	Disclaimer string        `json"disclaimer"`
-	Rates      map[string]float32    `json"rates"`
+	Id         		int        			`json:"id"`
+	Base       		string        		`json:"base"`
+	TimestampUnix  	int64        		`json:"timestamp"`
+	Disclaimer 		string        		`json"disclaimer"`
+	Rates      		map[string]float32  `json"rates"`
 }
 
 func (f Cources) ToString() string {
-	return fmt.Sprintf("Base; %s, Timestamp: %i", f.Base, f.Timestamp)
+	return fmt.Sprintf("Base: %s\nTimestamp: %v\nRates: %v", f.Base, f.Timestamp(), f.Rates)
+}
+func (f Cources) Timestamp() time.Time {
+	return time.Unix(f.TimestampUnix, 0).UTC()
+}
+func (f Cources) SetTimestamp(timestamp time.Time) {
+	f.TimestampUnix = timestamp.Unix()
 }
